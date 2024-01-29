c806dae164d02b831134c9faa7b943518a0feb72
alexander lozovoy <aalozzzovoy@gmail.com>
2024-01-29 15:11:52 +0300

    Practical_work_3.6

--- mysite/shopapp/models.py
+++ mysite/shopapp/models.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    class Meta:
+        ordering = ['price', 'name'] # сортировка по цене, затем по имени (от min r max, наоборот: с "-")
+        # db_tablr = 'tech_products' # указывает к какой таблице обращаться
+        # verbose_name_plural = 'products' # указывает как объявлять данные во множественном числе
+
+    # name - имя продукта, CharField - поле, max_length - макс. длина
+    name = models.CharField(max_length=100)
+    # description - описание продукта, TextField - большое текстовое поле
+    description = models.TextField(null=False, blank=True)
+    # price - цена, DecimalField - для работы с деньгами,
+    # default - по умолчанию, max_digits - макс. символов, decimal_places - символов после запятой
+    price = models.DecimalField(default=0, max_digits=8, decimal_places=2)
+    # discount - скидка
+    discount = models.SmallIntegerField(default=0)
+    # crested_at - сохранение информации о создании продукта, auto_now_add=True - автосохранение (при создании нового)
+    crested_at = models.DateTimeField(auto_now_add=True)
+    # archived - архивировано ли
+    archived = models.BooleanField(default=False)
+
+class Order(models.Model):
+     delivery_address = models.TextField(null=True, blank=True)
+     promocode = models.CharField(max_length=20, null=False, blank=True)
+     crested_at = models.DateTimeField(auto_now_add=True)
+
+     # on_delete=models.PROTECT - защита от удаления заказов и user
+     user = models.ForeignKey(User, on_delete=models.PROTECT)
+
+     products = models.ManyToManyField(Product, related_name='orders')
+
--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -1,12 +1,12 @@
+from django.contrib.auth.models import Group
+
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 from timeit import default_timer
 
+from .models import Product, Order
+
 def shop_index(request: HttpRequest):
-    # print(request.path)
-    # print(request.method)
-    # print(request.headers)
-    # return HttpResponse('<h1>Hello World!</h1>')
     products = [
         ('TV', 3000),
         ('Laptop', 5000),
@@ -17,3 +17,24 @@ def shop_index(request: HttpRequest):
         'products': products,
     }
     return render(request, 'shopapp/shop-index.html', context=context) #для шаблона
+
+
+def groups_list(request: HttpRequest):
+    context = {
+        'groups': Group.objects.prefetch_related('permissions').all(),
+    }
+    return render(request, 'shopapp/groups-list.html', context=context)  # для шаблона
+
+# prefetch_related('permissions') - оптимизация количества запросов
+
+def products_list(request: HttpRequest):
+    context = {
+        'products': Product.objects.all(),
+    }
+    return render(request, 'shopapp/products-list.html', context=context)  # для шаблона
+
+def orders_list(request: HttpRequest):
+    context = {
+        'orders': Order.objects.select_related('user').prefetch_related('products').all(), # select_related('user') - искл. лишние запросы
+    }
+    return render(request, 'shopapp/orders-list.html', context=context)  # для шаблона
