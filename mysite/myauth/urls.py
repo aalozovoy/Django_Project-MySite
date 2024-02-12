4ec24b198607866abd57dbef1598bf22b39a2f1b
alexander lozovoy <aalozzzovoy@gmail.com>
2024-02-12 17:09:20 +0300

    Practical_work_9.6

---
+++ mysite/myauth/urls.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.views import LoginView
+from django.urls import path
+# from .views import login_view
+
+from .views import (get_cookie_view,
+                    set_cookie_view,
+                    get_session_view,
+                    set_session_view,
+                    MyLogoutView,)
+
+
+app_name = "myauth"
+
+urlpatterns = [
+    path("login/",
+         LoginView.as_view(template_name="myauth/login.html",
+                           redirect_authenticated_user=True,
+                           ),
+        name="login"),
+    path("logout/", MyLogoutView.as_view(), name="logout"),
+    path("cookie/get/", get_cookie_view, name="cookie_get"),
+    path("cookie/set/", set_cookie_view, name="cookie_set"),
+    path("session/get/", get_session_view, name="session_get"),
+    path("session/set/", set_session_view, name="session_set"),
+]
+'''
+template_name="myauth/login.html" - необходимо указать шаблон (откорректировать шаблон {{ form.as_p }})
+LOGIN_REDIRECT_URL = '/admin/' - указывается в settings (куда переходить)
+redirect_authenticated_user=True - чтобы не вводить данные каждый раз
+'''
